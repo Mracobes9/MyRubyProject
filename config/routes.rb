bf59b7b3350bb41e438a90e316f544aa4bedbf68
Mracobes <kac.kurilov@yandex.ru>
2018-03-16 13:10:20 +0300

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'signup' =>'users#new'
 
   get 'home' => 'static_pages#home'
@@ -13,5 +15,10 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,25 +5,36 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   test "invalid signup information" do
+    user_params = { 
+      user: { 
+        name:  "",
+        email: "user@invalid",
+        password:              "foo",
+        password_confirmation: "bar" 
+      } 
+    }
+
     get signup_path
+    
     assert_no_difference 'User.count' do
-      post users_path, user: {
-        name: "",
-        email: "",
-        password:"",
-        password_confirmation:"" 
-      }
+      post users_path, params: { user: { name:  "",
+      email: "user@invalid",
+      password:              "foo",
+      password_confirmation: "bar" } }
     end
+    
     assert_template 'users/new'
   end
+  
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name:  "Example User",
-                                            email: "user@example.com",
-                                            password:             "password",
-                                            password_confirmation: "password" }
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
+    follow_redirect!
     assert_template 'users/show'
   end
 end
