b6dcd371454508126a8e8ea7ecf1254e6de21599
Mracobes <kac.kurilov@yandex.ru>
2018-03-26 08:31:15 +0300

    InProgress Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     VALID_EMAIL_REGEX = X = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     validates :name, presence: true,length: {maximum: 50}
 
     validates :email, presence:true,length: {maximum:255},format:{with: VALID_EMAIL_REGEX}, uniqueness:{case_sensitive: false}
@@ -41,6 +41,14 @@ class User < ApplicationRecord
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
     end
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest,User.digest(reset_token))
+        update_attribute(:reset_sent_at,Time.zone.now)
+    end
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
 private
 
     def create_activation_digest
@@ -51,5 +59,6 @@ private
     def downcase_email
         self.email.downcase!
     end
+    
     
 end
